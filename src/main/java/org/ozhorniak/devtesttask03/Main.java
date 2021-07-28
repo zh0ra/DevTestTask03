ab41670d01636ea17055be09b553bb698ce1f70b
zhora <zhora.mail@gmail.com>
2021-07-28 23:42:16 +0200

    Bugfix 1

--- src/main/java/org/ozhorniak/devtesttask03/Main.java
+++ src/main/java/org/ozhorniak/devtesttask03/Main.java
@@ -2,21 +2,9 @@ package org.ozhorniak.devtesttask03;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
+import org.ozhorniak.devtesttask03.UnionFind;
 
-/*The first line of input contains a positive number n , next n lines contains pairs of
-positive integers, where each pair identifies a connection between two vertices in a
-graph. Please provide a working code that will give us the answer for the following
-questions: how many separated graphs are in the input.
-Sample:
-For the input:
-3
-2 3
-1 2
-5 6
-expected output is: 2
-*/
 /**
  * 
  * @author Oleksandr Zhorniak
@@ -39,29 +27,29 @@ public class Main {
 	}
 
 	private void taskTest() throws FileNotFoundException {
-		Scanner in = new Scanner(new File("input_large.txt"));
+		//Scanner in = new Scanner(new File("input_large.txt"));
 
-		//Scanner in = new Scanner(System.in);
-		
-		int n = in.nextInt();
-		// System.out.println("n = " + n);
-		UnionFind un = new UnionFind(n);
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			// System.out.println("n = " + n);
+			UnionFind un = new UnionFind(n);
 
-		int p;
-		int q;
+			int p;
+			int q;
 
-		while (in.hasNext()) {
+			while (in.hasNext()) {
 
-			p = in.nextInt();
-			q = in.nextInt();
+				p = in.nextInt();
+				q = in.nextInt();
 
-			if (un.find(p) == un.find(q))
-				continue;
-			un.union(p, q);
-			System.out.println(p + " " + q);
-		
+				if (un.find(p) == un.find(q))
+					continue;
+				un.union(p, q);
+				System.out.println(p + " " + q);
+			
+			}
+
+			System.out.println("Count of separated graphs is : " + un.getCounder());
 		}
-
-		System.out.println("Count of separated graphs is : " + un.getCounder());
 	}
 }
